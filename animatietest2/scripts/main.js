77d2790fe424db175d37fe5f2639d5c350c04026
cgheydra <chris@192.168.2.111>
2017-03-04 14:51:10 +0100

    nog een test example toegevoegd
    Je kunt ook this gebruiken in die .to functie. Biedt mogelijkheden

---
+++ animatietest2/scripts/main.js
@@ -0,0 +1,17 @@
+
+var doEnterAnim = function() {
+	var tl1 = new TimelineMax();
+	tl1.to(this, 0.5, {marginLeft: 50, color: '#99AAFF', ease: Bounce.easeOut});
+};
+
+var doOutAnim = function() {
+	var tl1 = new TimelineMax();
+	tl1.to(this, 0.5, {marginLeft: 0, color: '#FFFFFF', ease: Bounce.easeOut});
+};
+
+var nodes = document.getElementsByClassName('menu');
+for(var i=0; i<nodes.length; i++){
+	nodes[i].onmouseenter = doEnterAnim;
+	nodes[i].onmouseout = doOutAnim;
+}
+
