71cdc1da97200d53d51d9d5dedf6d516d60c538b
Tim van den Bosch <tim@afriendofmine.nl>
2017-03-05 18:51:19 +0100

    Removed anwser

--- wk4exercises/wk4exercise07/scripts/main_answer.js
+++ wk4exercises/wk4exercise07/scripts/main_answer.js
@@ -1,4 +1,35 @@
 
-document.getElementById('explode').onclick = function() {
-	explodeLetters();
+// set the timeline
+var timeline = new TimelineMax();
+
+// bind events
+document.getElementById('boom').onclick = function() {
+
+	for (var i = 0; i < 8; i++) {
+		explodeLetters(i);
+	}
 };
+
+// set function
+function explodeLetters(number) {
+
+	// set random sizes
+	var randomX = Math.floor(Math.random() * 800),
+		randomY = Math.floor(Math.random() * 300),
+		plusOrMinus = Math.random() < 0.5 ? '-' : '+';
+
+	timeline.to('.letter', 1, { 
+		scale: 3, 
+		color: 'yellow', 
+		margin: 20 
+	})
+	.to('.explode0'+ number, 1, { 
+		y: plusOrMinus +'='+ randomY, 
+		x: plusOrMinus +'='+ randomX, 
+		rotation: -360, 
+		ease: Power2.EaseOut, 
+		scale: 6,
+		opacity: 0,
+		color: 'red',
+	}, 'explode-=.1');
+}
